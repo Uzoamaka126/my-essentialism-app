fdfca6a191c7180084434fc45ed39167f7e04f4c
Uzoamaka126 <vanessapeters126@gmail.com>
2019-11-25 12:42:30 +0100

    Added new models for the application

---
+++ models/value-model.js
@@ -0,0 +1,63 @@
+const db = require('../database/db-config');
+const userModel = require('./users-models');
+
+module.exports = {
+    get,
+    getById,
+    getByUserId,
+    getBy,
+    addValue,
+    getUserValues,
+    addUserValue
+}
+
+function get() {
+    return db('values')
+        .select('id', 'value_name')
+}
+
+function getById(id) {
+    return db('values')
+        .select('id', 'value_name')
+        .where({ id })
+        .first()
+}
+
+function getByUserId(id) {
+    return db('users')
+        .select('id')
+        .where({ id })
+        .first()
+}
+
+function getBy(filter) {
+    return db('values')
+        .where(filter)
+}
+
+function addValue(value) {
+    return db('values')
+        .insert(value)
+        .then((ids => {
+            return getById(ids[0]);
+        }));
+}
+
+function addUserValue(value, userId) {
+    return db('users_and_values as uv')
+        .insert(value, 'id')
+        .select('users as u', 'uv.user_id', 'uv.value_id')
+        .select('values as v')
+        .where({ 'u.id': userId })
+        .then((ids => {
+            return getByUserId(ids[0]);
+        }));
+}
+
+function getUserValues(userId) {
+    return db('users_and_values as uv')
+        .join('users as u', 'u.id', 'uv.user_id')
+        .join('values as v', 'v.id', 'uv.value_id')
+        .select('v.id', 'u.id', 'v.value_name', 'u.username')
+        .where({ 'u.id': userId })
+}
