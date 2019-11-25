1a07c5a0ed4c8f21ed8b02080e5d3cb28ea29c3f
Uzoamaka126 <vanessapeters126@gmail.com>
2019-11-25 12:38:04 +0100

    Setup of the application server

--- api/server.js
+++ api/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const helmet = require('helmet');
 const cors = require('cors');
+const helmet = require('helmet');
+const authRouter = require('../routers/auth-router');
+const usersRouter = require('../routers/user-router');
+const valuesRouter = require('../routers/value-routers');
+
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+server.use('/api/values', valuesRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({
