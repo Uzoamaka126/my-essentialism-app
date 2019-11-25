6f9366bb6e92911efe1cdfed2e3e9aa8e779911a
Uzoamaka126 <vanessapeters126@gmail.com>
2019-11-25 12:37:44 +0100

    Added the knex file for the db

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+module.exports = {
+    development: {
+      client: 'sqlite3',
+      connection: { 
+        filename: './database/users.db3' 
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations'
+      },
+      seeds: { 
+        directory: './database/seeds' 
+      },
+    },
+    test: {
+      client: 'sqlite3',
+      connection: {
+        filename: './database/test.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+      },
+      seeds: {
+        directory: './database/seeds',
+      }
+    },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: './database/migrations',
+      },
+      seeds: {
+        directory: './database/seeds',
+      },
+    }
+  }
+  
