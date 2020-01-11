f90b84c9de356eb0bdcfd6b9a1c16acdbc26bbc7
Uzoamaka126 <vanessapeters126@gmail.com>
2020-01-11 23:45:57 +0100

    ft [Added new seed to popilate the values table] uzoamaka-anyanwu

--- client/src/redux-store/actions/auth.js
+++ client/src/redux-store/actions/auth.js
@@ -1,2 +1,25 @@
 import axios from 'axios';
 import * as types from './actionTypes';
+import { apiURL } from '../../_helpers/urls';
+
+export const register = user => dispatch => {
+    dispatch({
+        type: types.REGISTER_REQUEST
+    });
+    axios
+        .post(`${apiURL}/auth/register`, user)
+        .then(res => {
+            dispatch({
+                type: types.REGISTER_SUCCESS,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({
+                type: types.REGISTER_FAILURE
+            });
+        }
+};
+
+
