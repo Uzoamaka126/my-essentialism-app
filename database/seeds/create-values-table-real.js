96afb78ed3702ee47012ffbfdd5ce92ed95d5e95
Uzoamaka126 <vanessapeters126@gmail.com>
2020-01-19 01:50:12 +0100

    added users seed file

--- database/seeds/create-values-table-real.js
+++ database/seeds/create-values-table-real.js
@@ -1,46 +1,46 @@
 
 exports.seed = function(knex) {
       // Inserts seed entries
-      return knex('values').insert([
-        {
-          value_name: "Athletic Ability"
-        },
-        {
-          value_name: "Arts and Literature"
-        },
-        {
-          value_name: "Career Growth"
-        },
-        {
-          value_name: "Creativity"
-        },
-        {
-          value_name: "Kindness and Generousity"
-        },
-        {
-          value_name: "Living in the moment"
-        },
-        {
-          value_name: "Membership in a social group"
-        },
-        {
-          value_name: "My community"
-        },
-        {
-          value_name: "Moral Pricniples"
-        }
-        ,
-        {
-          value_name: "Nature and the environment"
-        },
-        {
-          value_name: "Others"
-        },
-        {
-          value_name: "Relationships with Friends and Family"
-        },
-        {
-          value_name: "Sense of Humour"
-        }
-      ]);
+  return knex('values').insert([
+    {
+      value_name: "Athletic Ability"
+    },
+    {
+      value_name: "Arts and Literature"
+    },
+    {
+      value_name: "Career Growth"
+    },
+    {
+      value_name: "Creativity"
+    },
+    {
+      value_name: "Kindness and Generousity"
+    },
+    {
+      value_name: "Living in the moment"
+    },
+    {
+      value_name: "Membership in a social group"
+    },
+    {
+      value_name: "My community"
+    },
+    {
+      value_name: "Moral Pricniples"
+    }
+    ,
+    {
+      value_name: "Nature and the environment"
+    },
+    {
+      value_name: "Others"
+    },
+    {
+      value_name: "Relationships with Friends and Family"
+    },
+    {
+      value_name: "Sense of Humour"
+    }
+  ]);
 };
